ef93fb0a7ebbf35d9d36df411ccae7ebd501e07f
Dmytro Ileiko <deamon999@gmail.com>
2026-08-26 13:53:04 -0500

    State converted to dropdown

---
+++ HrPlatform/Utils/USStates.cs
@@ -0,0 +1,27 @@
+namespace HrPlatform.Utils;
+
+public static class USStates
+{
+    public static readonly Dictionary<string, string> States = new()
+    {
+        {"AL", "Alabama"}, {"AK", "Alaska"}, {"AZ", "Arizona"}, {"AR", "Arkansas"},
+        {"CA", "California"}, {"CO", "Colorado"}, {"CT", "Connecticut"}, {"DE", "Delaware"},
+        {"FL", "Florida"}, {"GA", "Georgia"}, {"HI", "Hawaii"}, {"ID", "Idaho"},
+        {"IL", "Illinois"}, {"IN", "Indiana"}, {"IA", "Iowa"}, {"KS", "Kansas"},
+        {"KY", "Kentucky"}, {"LA", "Louisiana"}, {"ME", "Maine"}, {"MD", "Maryland"},
+        {"MA", "Massachusetts"}, {"MI", "Michigan"}, {"MN", "Minnesota"}, {"MS", "Mississippi"},
+        {"MO", "Missouri"}, {"MT", "Montana"}, {"NE", "Nebraska"}, {"NV", "Nevada"},
+        {"NH", "New Hampshire"}, {"NJ", "New Jersey"}, {"NM", "New Mexico"}, {"NY", "New York"},
+        {"NC", "North Carolina"}, {"ND", "North Dakota"}, {"OH", "Ohio"}, {"OK", "Oklahoma"},
+        {"OR", "Oregon"}, {"PA", "Pennsylvania"}, {"RI", "Rhode Island"}, {"SC", "South Carolina"},
+        {"SD", "South Dakota"}, {"TN", "Tennessee"}, {"TX", "Texas"}, {"UT", "Utah"},
+        {"VT", "Vermont"}, {"VA", "Virginia"}, {"WA", "Washington"}, {"WV", "West Virginia"},
+        {"WI", "Wisconsin"}, {"WY", "Wyoming"}, {"DC", "District of Columbia"}
+    };
+
+    public static string GetStateName(string? abbreviation)
+    {
+        if (string.IsNullOrWhiteSpace(abbreviation)) return string.Empty;
+        return States.TryGetValue(abbreviation.ToUpper(), out var name) ? name : abbreviation;
+    }
+}
